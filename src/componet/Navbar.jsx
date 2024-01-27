82176fd3412e8c76e3a6dae3c66a0949ad125040
Ranjeet0062 <105407521+Ranjeet0062@users.noreply.github.com>
2024-01-27 21:36:26 +0530

    Update Navbar.jsx

--- src/componet/Navbar.jsx
+++ src/componet/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import logoFullLight from "../assets/Logo/Logo-Full-Light.png"
 function Navbar() {
-  return (
-    <div className='w-[100%] h-[60px] bg-pink-800 text-white'>
-        <h1>this is navbar</h1>
-    </div>
-  )
+    return (
+        <div className='w-[100%] h-[60px] text-white bg-caribbeangreen-300 flex items-center justify-center '>
+            <div className='flex justify-around w-[100%]'>
+                <div>
+                    <img src={logoFullLight} alt="logo" />
+                </div>
+                <div>hdgsfjk</div>
+                <div>efghjk</div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
