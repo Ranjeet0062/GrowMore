7626593575de1e3ed01a78fe4e5106786266d8b3
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-02-09 17:12:13 +0530

    try through cli

--- src/api/api.js
+++ src/api/api.js
@@ -3,5 +3,6 @@ export const authapi={
     login:`${BASE_URL}/user/api/login`,
     signup:`${BASE_URL}/user/api/signup`,
     sendotp:`${BASE_URL}/user/api/sendotp`,
-    resetpassword:`${BASE_URL}/user/api/sendresetPsswordToken`
+    resetpassword:`${BASE_URL}/user/api/sendresetPsswordToken`,
+    sendtoken:`${BASE_URL}`
 }
