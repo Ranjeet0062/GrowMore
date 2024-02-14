2a2583bd1fbb270f7d1ce44f09947a2d120f251f
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-02-14 19:06:17 +0530

    d

--- src/pages/ContactUS.jsx
+++ src/pages/ContactUS.jsx
@@ -6,7 +6,6 @@ import ContactUsDetail from "../componet/form/ContactUsDetail"
 function ContactUS() {
   return (
     <div className='text-white'>
-      <Navbar />
       <div className='max-w-[1200px] mt-[30px] mb-[30px] flex flex-col md:flex-row  justify-between mx-auto '>
        <ContactUsDetail/>
         <ContactForm />
