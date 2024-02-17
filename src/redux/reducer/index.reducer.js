d925e880e6c220c616c39478c21788e1c3644f4f
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-02-17 17:15:06 +0530

    cart reducer is created

--- src/redux/reducer/index.reducer.js
+++ src/redux/reducer/index.reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slices/auth.slice"
 import profileSlice from"../slices/profile.slice"
+import cartSlice from "../slices/cart.slice"
 const rootReducers=combineReducers({
     auth:authReducer,
-    profile:profileSlice
+    profile:profileSlice,
+    cart:cartSlice
 })
 export default rootReducers
