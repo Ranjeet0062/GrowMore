759ab48baa232adc6f05ebbec445e9abb62fdb26
Ranjeet0062 <odedararanjeet1361@gmail.com>
2024-04-01 22:32:21 +0530

    cookie is set currectly

--- backend/controller/signup.controller.js
+++ backend/controller/signup.controller.js
@@ -151,6 +151,7 @@ exports.login = async (req, res) => {
 			const options = {
 				expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
 				httpOnly: true,
+				secure: true
 			};
 			res.cookie("token", token, options)
 			return res.status(200).json({
